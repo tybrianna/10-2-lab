0dd42e25a05aa71aaf926bec8236fed1d2c0a4d6
tybrianna <tybriannahall@gmail.com>
2026-05-07 09:47:52 -0400

    updated app.tsx to include decounce component

--- day2/lab2/lab2/src/components/app.tsx
+++ day2/lab2/lab2/src/components/app.tsx
@@ -1,11 +1,11 @@
 import PaginationDemo from "./Hook/PaginationDemo";
-
+import DebounceSearchDemo from "./Hook/DebounceSearchDemo";
 function App() {
   return (
     <div>
       <PaginationDemo />
+      <DebounceSearchDemo />
     </div>
   );
 }
-
 export default App;
